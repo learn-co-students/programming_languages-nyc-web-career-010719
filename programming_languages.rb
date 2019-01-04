7a846ddd7648e40af668e07884c838460d037261
Eli Cleveland <elicleveland12@gmail.com>
2019-01-04 18:35:43 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,49 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages[:oo].each do |language, type|
+    if language == :ruby 
+      new_hash[:ruby] = {
+        type: "interpreted",
+        style: [:oo]
+      }
+    elsif language == :javascript 
+      new_hash[:javascript] = {
+        type: "interpreted",
+        style: [:oo, :functional]
+      }
+    elsif language == :python 
+      new_hash[:python] = {
+        type: "interpreted",
+        style: [:oo]
+      }
+    elsif language == :java 
+      new_hash[:java] = {
+        type: "compiled",
+        style: [:oo]
+      }
+    end 
+  end 
+  languages[:functional].each do |language, type|    
+    if language == :clojure
+      new_hash[:clojure] = {
+        type: "compiled",
+        style: [:functional]
+      }
+    elsif language == :erlang 
+      new_hash[:erlang] = {
+        type: "compiled",
+        style: [:functional]
+      }
+    elsif language == :scala
+      new_hash[:scala] = {
+        type: "compiled",
+        style: [:functional]
+      }
+    end 
+  end
+  new_hash
 end
+
+
+
+
